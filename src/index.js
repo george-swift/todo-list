d7a57c27bfe4dc3e380f506f519d5a88a39c247a
ybadmus <yusif.badmus@gmail.com>
2021-05-19 21:28:30 +0100

    add event to show add project form dynamically

--- src/index.js
+++ src/index.js
@@ -3,31 +3,31 @@ import { render, addProject, createPrompt } from './module/projectModule.js';
 import { newProjectField } from './module/domManager.js';
 
 const parentDiv = document.querySelector('.accordion-body');
-const prompt = createPrompt();
 
 function init() {
-  const menuList = document.querySelector('#menu-items');
-  const defaultProject = render('Default Project', new Date().toLocaleString().split(',')[0]);
+    const menuList = document.querySelector('#menu-items');
+    const defaultProject = render('Default', new Date().toLocaleString().split(',')[0]);
+    const prompt = createPrompt();
 
-  menuList.append(defaultProject, prompt);
-  return menuList;
+    menuList.append(defaultProject, prompt);
+    return menuList;
 }
 
 function addNewProject() {
-  const menuList = document.querySelector('#menu-items');
-  const newProject = addProject('Another Project');
-  menuList.insertBefore(newProject, prompt);
+    const prompt = createPrompt();
+    const menuList = document.querySelector('#menu-items');
+    const newProject = addProject('New');
+    menuList.insertBefore(newProject, prompt);
 }
 
-const newProjectBtn = document.querySelector('#newProjectBtn');
+// const newProjectBtn = document.querySelector('#newProjectBtn');
 
-newProjectBtn.addEventListener('click', () => {
-  const field = newProjectField();
-  field.classList.toggle('d-block');
-  newProjectBtn.classList.add('d-none');
-  parentDiv.appendChild(field);
-});
+// newProjectBtn.addEventListener('click', () => {
+//   const field = newProjectField();
+//   field.classList.toggle('d-block');
+//   newProjectBtn.classList.add('d-none');
+//   parentDiv.appendChild(field);
+// });
 
 init();
-
-addNewProject();
+//addNewProject();
--- src/module/domManager.js
+++ src/module/domManager.js
@@ -1,53 +1,54 @@
 const attrs = {
-  wrap: {
-    class: 'd-flex w-100',
-  },
+    wrap: {
+        class: 'd-flex w-100',
+    },
 
-  input: {
-    class: 'form-control w-100',
-    id: 'projectName',
-  },
+    input: {
+        class: 'form-control w-100',
+        id: 'projectName',
+    },
 
-  addBtn: {
-    class: 'btn btn-success text-white me-3',
-    id: 'addProjectBtn',
-  },
+    addBtn: {
+        class: 'btn btn-success text-white me-3',
+        id: 'addProjectBtn',
+    },
 
-  cancelBtn: {
-    class: 'btn btn-danger text-white',
-    id: 'cancelBtn',
-  },
+    cancelBtn: {
+        class: 'btn btn-danger text-white',
+        id: 'cancelBtn',
+    },
 };
 
 const customAttributes = (elem, attrs) => {
-  Object.entries(attrs).forEach((key, value) => {
-    elem.setAttribute(key, value);
-  });
+    Object.entries(attrs).forEach(([key, value]) => {
+        elem.setAttribute(key, value);
+    });
 };
 
 const newProjectField = () => {
-  const div = document.createElement('div');
-  div.classList.add('d-none');
+    const div = document.createElement('div');
+    div.classList.add('d-none');
+    div.setAttribute('id', 'addProjectDiv');
 
-  const input = document.createElement('input');
-  customAttributes(input, attrs.input);
+    const input = document.createElement('input');
+    customAttributes(input, attrs.input);
 
-  const wrap = document.createElement('div');
-  customAttributes(wrap, attrs.wrap);
+    const wrap = document.createElement('div');
+    customAttributes(wrap, attrs.wrap);
 
-  const add = document.createElement('button');
-  customAttributes(add, attrs.addBtn);
-  add.textContent = 'Add';
+    const add = document.createElement('button');
+    customAttributes(add, attrs.addBtn);
+    add.textContent = 'Add';
 
-  const cancel = document.createElement('button');
-  customAttributes(cancel, attrs.cancelBtn);
-  cancel.textContent = 'Cancel';
+    const cancel = document.createElement('button');
+    customAttributes(cancel, attrs.cancelBtn);
+    cancel.textContent = 'Cancel';
 
-  wrap.append(add, cancel);
+    wrap.append(add, cancel);
 
-  div.append(input, wrap);
+    div.append(input, wrap);
 
-  return div;
+    return div;
 };
 
 export { newProjectField };
