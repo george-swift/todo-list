5b1eb95dfadc8c92784d6dd3330620e551ae6516
George <71434803+george-swift@users.noreply.github.com>
2021-05-19 21:34:44 +0100

    Include functions to add new project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-//import {render} from './module/todoModule.js';
-import { render } from './module/projectModule.js';
+// import {render} from './module/todoModule.js';
+import { render, addProject, createPrompt } from './module/projectModule.js';
+import { newProjectField } from './module/domManager.js';
+
+const parentDiv = document.querySelector('.accordion-body');
+const prompt = createPrompt();
 
 function init() {
-    const menuList = document.querySelector('#menu-items');
-    console.log(menuList);
-    const defaultProject = render('Default Project', new Date().toLocaleString().split(",")[0]);
+  const menuList = document.querySelector('#menu-items');
+  const defaultProject = render('Default Project', new Date().toLocaleString().split(',')[0]);
 
-    menuList.appendChild(defaultProject);
-    return menuList;
+  menuList.append(defaultProject, prompt);
+  return menuList;
 }
 
+function addNewProject() {
+  const menuList = document.querySelector('#menu-items');
+  const newProject = addProject('Another Project');
+  menuList.insertBefore(newProject, prompt);
+}
+
+const newProjectBtn = document.querySelector('#newProjectBtn');
+
+newProjectBtn.addEventListener('click', () => {
+  const field = newProjectField();
+  field.classList.toggle('d-block');
+  newProjectBtn.classList.add('d-none');
+  parentDiv.appendChild(field);
+});
+
 init();
+
+addNewProject();
--- src/module/projectModule.js
+++ src/module/projectModule.js
@@ -1,23 +1,68 @@
-const render = (name, dateCreated) => {
-    const option = document.createElement('li');
-    const classes = ["option", "center"];
-    option.classList.add(...classes);
+const render = (name) => {
+  const option = document.createElement('li');
+  const classes = ['option', 'center'];
+  option.classList.add(...classes);
 
-    const anchor = document.createElement('a');
-    anchor.href = '#';
+  const anchor = document.createElement('a');
+  anchor.href = '#';
 
-    const image = document.createElement('img');
-    image.src = 'assets/push-pin.svg';
-    image.className = 'project-icon';
+  const image = document.createElement('img');
+  image.src = 'assets/push-pin.svg';
+  image.className = 'project-icon';
 
-    const text = document.createElement('span');
-    text.textContent = name;
+  const text = document.createElement('span');
+  text.textContent = name;
 
-    anchor.appendChild(image);
-    anchor.appendChild(text);
-    option.appendChild(anchor);
+  anchor.appendChild(image);
+  anchor.appendChild(text);
+  option.appendChild(anchor);
 
-    return option;
-}
+  return option;
+};
 
-export { render };
+const createPrompt = () => {
+  const option = document.createElement('li');
+  const classes = ['option', 'center'];
+  option.classList.add(...classes);
+
+  const anchor = document.createElement('a');
+  anchor.setAttribute('id', 'newProjectBtn');
+
+  const image = document.createElement('img');
+  image.src = 'assets/plus.svg';
+  image.className = 'project-icon';
+
+  const text = document.createElement('span');
+  text.textContent = 'Add new project';
+
+  anchor.appendChild(image);
+  anchor.appendChild(text);
+  option.appendChild(anchor);
+
+  return option;
+};
+
+const addProject = (project) => {
+  const elem = createItem(project);
+  return elem;
+};
+
+const createItem = (project) => {
+  const option = document.createElement('li');
+  const classes = ['option', 'center'];
+  option.classList.add(...classes);
+
+  const anchor = document.createElement('a');
+  anchor.href = '#';
+  anchor.classList.add('project-name');
+
+  const text = document.createElement('span');
+  text.textContent = project;
+
+  anchor.appendChild(text);
+  option.appendChild(anchor);
+
+  return option;
+};
+
+export { render, addProject, createPrompt };
