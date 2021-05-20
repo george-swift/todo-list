50b9f0fb0f17dde6deda67df6b1fced9fa700228
George <71434803+george-swift@users.noreply.github.com>
2021-05-20 16:46:48 +0100

    Add styling for sidebar responsiveness

--- src/module/domManager.js
+++ src/module/domManager.js
@@ -1,27 +1,49 @@
-const attributes = (function() {
-    return {
-        div: {
-            class: 'd-none p-2 w-100',
-            id: 'addProjectForm',
-        },
-        wrap: {
-            class: 'd-flex w-100 mt-2 wrap-btns',
-        },
-        input: {
-            class: 'form-control',
-            id: 'projectName',
-        },
-        addBtn: {
-            class: 'btn btn-success text-white w-48',
-            id: 'addProjectBtn',
+const attributes = (() => ({
+  div: {
+    class: 'd-none wrapper',
+    id: 'addProjectForm',
+  },
 
-        },
-        cancelBtn: {
-            class: 'btn btn-danger text-white w-48',
-            id: 'cancelBtn',
-            textContent: 'Cancel'
-        },
-    };
-})();
+  li: {
+    class: 'option center',
+  },
 
-export { attributes };
+  a: {
+    all: { href: '#' },
+    new: { id: 'newProjectBtn' },
+    created: { class: 'project-name' },
+  },
+
+  icon: {
+    default: {
+      class: 'project-icon',
+      src: 'assets/push-pin.svg',
+    },
+    new: {
+      class: 'project-icon',
+      src: 'assets/plus.svg',
+    },
+  },
+
+  wrap: {
+    class: 'd-flex w-100 mt-2 wrap-btns',
+  },
+
+  input: {
+    class: 'form-control',
+    id: 'projectName',
+  },
+
+  addBtn: {
+    class: 'btn btn-success text-white w-48',
+    id: 'addProjectBtn',
+  },
+
+  cancelBtn: {
+    class: 'btn btn-danger text-white w-48',
+    id: 'cancelBtn',
+    textContent: 'Cancel',
+  },
+}))();
+
+export default attributes;
