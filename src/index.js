9e68d9794569fe936b4371a1ffbe97493014a182
ybadmus <yusif.badmus@gmail.com>
2021-05-24 16:32:48 +0100

    begin to add logic edit feature

--- src/index.js
+++ src/index.js
@@ -1,152 +1,155 @@
 import './style.css';
 
 import {
-  createDefaultProject,
-  newProjectButton,
-  createProject,
-  wrapProject,
+    createDefaultProject,
+    newProjectButton,
+    createProject,
+    wrapProject,
 } from './module/projectModule.js';
 
 import {
-  createCards,
+    createCards,
 } from './module/todoModule.js';
 
 function Todo(id, title, description, dueDate, priority, project) {
-  this.id = id;
-  this.title = title;
-  this.description = description;
-  this.dueDate = dueDate;
-  this.priority = priority;
-  this.projectName = project;
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.projectName = project;
 }
 
 const addToSelect = (project) => {
-  const select = document.querySelector('#todoProjectList');
-  const name = wrapProject(project);
-  select.appendChild(name);
+    const select = document.querySelector('#todoProjectList');
+    const name = wrapProject(project);
+    select.appendChild(name);
 };
 
 const addNewProject = (project) => {
-  const projectButtonDiv = document.querySelector('#newProjectBtn').parentNode;
-  const parentDiv = document.querySelector('#menu-items');
+    const projectButtonDiv = document.querySelector('#newProjectBtn').parentNode;
+    const parentDiv = document.querySelector('#menu-items');
 
-  parentDiv.insertBefore(createProject(project), projectButtonDiv);
-  addToSelect(project);
+    parentDiv.insertBefore(createProject(project), projectButtonDiv);
+    addToSelect(project);
 };
 
 const superToggle = (elem, old, curr) => {
-  if (!elem.classList.contains(old)) return;
-  elem.classList.remove(old);
-  elem.classList.add(curr);
+    if (!elem.classList.contains(old)) return;
+    elem.classList.remove(old);
+    elem.classList.add(curr);
 };
 
-const initialize = (projects = JSON.parse(localStorage.getItem('project-collection'))
-  ? JSON.parse(localStorage.getItem('project-collection')) : [], todos = JSON.parse(localStorage.getItem('todo-collection'))
-  ? JSON.parse(localStorage.getItem('todo-collection')).filter((todo) => todo.projectName.toLowerCase() === 'default') : []) => {
-  const menuList = document.querySelector('#menu-items');
-  const defaultProject = createDefaultProject('Default');
-  const newProjectBtn = newProjectButton();
-  menuList.append(defaultProject, newProjectBtn);
+const initialize = (projects = JSON.parse(localStorage.getItem('project-collection')) ?
+    JSON.parse(localStorage.getItem('project-collection')) : [], todos = JSON.parse(localStorage.getItem('todo-collection')) ?
+    JSON.parse(localStorage.getItem('todo-collection')).filter((todo) => todo.projectName.toLowerCase() === 'default') : []) => {
+    const menuList = document.querySelector('#menu-items');
+    const defaultProject = createDefaultProject('Default');
+    const newProjectBtn = newProjectButton();
+    menuList.append(defaultProject, newProjectBtn);
 
-  projects.forEach((project) => addNewProject(project));
+    projects.forEach((project) => addNewProject(project));
 
-  const todoapp = document.querySelector('#todo-app');
-  todoapp.appendChild(createCards(todos));
+    const todoapp = document.querySelector('#todo-app');
+    todoapp.appendChild(createCards(todos));
 };
 
 initialize();
 
 const saveNewToDo = (item) => {
-  const todos = JSON.parse(localStorage.getItem('todo-collection'))
-    ? JSON.parse(localStorage.getItem('todo-collection')) : [];
-  todos.push(item);
-  localStorage.setItem('todo-collection', JSON.stringify(todos));
-
-  document.getElementById('todo-app').innerHTML = '';
-  const todoapp = document.querySelector('#todo-app');
-  todoapp.appendChild(createCards(todos.filter((todo) => todo.projectName.toLowerCase() === item.projectName.toLowerCase())));
-
-  document.getElementById('tabTitle').innerHTML = '';
-  const tabTitle = document.querySelector('#tabTitle');
-  tabTitle.textContent = item.projectName.toLowerCase();
-};
-
-const resetFields = (...fields) => {
-  fields.forEach((field) => {
-    field.value = '';
-  });
-};
-
-document.addEventListener('click', (event) => {
-  if (event.target.id === 'addProjectBtn') {
-    if (document.getElementById('projectName').value.trim().length > 4) {
-      const projects = JSON.parse(localStorage.getItem('project-collection'))
-        ? JSON.parse(localStorage.getItem('project-collection')) : [];
-      projects.push(document.getElementById('projectName').value);
-      addNewProject(document.getElementById('projectName').value);
-      localStorage.setItem('project-collection', JSON.stringify(projects));
-      document.getElementById('projectName').value = '';
-    }
-  }
-
-  if (event.target.id === 'cancelBtn') {
-    document.getElementById('projectName').value = '';
-
-    const field = document.querySelector('#newProjectBtn');
-    superToggle(field, 'd-none', 'd-block');
-
-    const adjacentDiv = document.querySelector('#addProjectForm');
-    superToggle(adjacentDiv, 'd-block', 'd-none');
-  }
-
-  if (event.target.parentNode.className === 'project-name' || event.target.parentNode.className === 'project-name-default') {
-    const todos = JSON.parse(localStorage.getItem('todo-collection'))
-      .filter((project) => project.projectName.toLowerCase() === event.target.id.split('-')[1]);
+    const todos = JSON.parse(localStorage.getItem('todo-collection')) ?
+        JSON.parse(localStorage.getItem('todo-collection')) : [];
+    todos.push(item);
+    localStorage.setItem('todo-collection', JSON.stringify(todos));
 
     document.getElementById('todo-app').innerHTML = '';
     const todoapp = document.querySelector('#todo-app');
-    todoapp.appendChild(createCards(todos));
+    todoapp.appendChild(createCards(todos.filter((todo) => todo.projectName.toLowerCase() === item.projectName.toLowerCase())));
 
     document.getElementById('tabTitle').innerHTML = '';
     const tabTitle = document.querySelector('#tabTitle');
-    tabTitle.textContent = event.target.id.split('-')[1];
-  }
+    tabTitle.textContent = item.projectName.toLowerCase();
+};
 
-  if (event.target.id.includes('delete-')) {
-    const todos = JSON.parse(localStorage.getItem('todo-collection'));
-    todos.splice(todos.indexOf(todos.filter((todo) => todo.id === parseInt(event.target.id.split('-')[1]))));
-    localStorage.setItem('todo-collection', JSON.stringify(todos));
+const resetFields = (...fields) => {
+    fields.forEach((field) => {
+        field.value = '';
+    });
+};
 
-    document.getElementById('todo-app').innerHTML = '';
-    const todoapp = document.querySelector('#todo-app');
-    todoapp.appendChild(createCards(todos.filter((todo) => todo.projectName.toLowerCase() === document.querySelector('#tabTitle').textContent.toLowerCase())));
-  }
+document.addEventListener('click', (event) => {
+    if (event.target.id === 'addProjectBtn') {
+        if (document.getElementById('projectName').value.trim().length > 4) {
+            const projects = JSON.parse(localStorage.getItem('project-collection')) ?
+                JSON.parse(localStorage.getItem('project-collection')) : [];
+            projects.push(document.getElementById('projectName').value);
+            addNewProject(document.getElementById('projectName').value);
+            localStorage.setItem('project-collection', JSON.stringify(projects));
+            document.getElementById('projectName').value = '';
+        }
+    }
 
-  if (event.target.id.includes('edit-')) {
-    const todos = JSON.parse(localStorage.getItem('todo-collection'));
-    todo = todos.filter((todo) => todo.id === parseInt(event.target.id.split('-')[1]));
+    if (event.target.id === 'cancelBtn') {
+        document.getElementById('projectName').value = '';
 
-    localStorage.setItem('todo-collection', JSON.stringify(todos));
+        const field = document.querySelector('#newProjectBtn');
+        superToggle(field, 'd-none', 'd-block');
 
-    document.getElementById('todo-app').innerHTML = '';
-    const todoapp = document.querySelector('#todo-app');
-    todoapp.appendChild(createCards(todos.filter((todo) => todo.projectName.toLowerCase() === document.querySelector('#tabTitle').textContent.toLowerCase())));
-  }
+        const adjacentDiv = document.querySelector('#addProjectForm');
+        superToggle(adjacentDiv, 'd-block', 'd-none');
+    }
+
+    if (event.target.parentNode.className === 'project-name' || event.target.parentNode.className === 'project-name-default') {
+        const todos = JSON.parse(localStorage.getItem('todo-collection'))
+            .filter((project) => project.projectName.toLowerCase() === event.target.id.split('-')[1]);
+
+        document.getElementById('todo-app').innerHTML = '';
+        const todoapp = document.querySelector('#todo-app');
+        todoapp.appendChild(createCards(todos));
+
+        document.getElementById('tabTitle').innerHTML = '';
+        const tabTitle = document.querySelector('#tabTitle');
+        tabTitle.textContent = event.target.id.split('-')[1];
+    }
+
+    if (event.target.id.includes('delete-')) {
+        const todos = JSON.parse(localStorage.getItem('todo-collection'));
+        todos.splice(todos.indexOf(todos.filter((todo) => todo.id === parseInt(event.target.id.split('-')[1]))));
+        localStorage.setItem('todo-collection', JSON.stringify(todos));
+
+        document.getElementById('todo-app').innerHTML = '';
+        const todoapp = document.querySelector('#todo-app');
+        todoapp.appendChild(createCards(todos.filter((todo) => todo.projectName.toLowerCase() === document.querySelector('#tabTitle').textContent.toLowerCase())));
+    }
+
+    if (event.target.id.includes('edit-')) {
+        const todos = JSON.parse(localStorage.getItem('todo-collection'));
+        const todo = todos.filter((todo) => todo.id === parseInt(event.target.id.split('-')[1]));
+        const todoIndex = todos.indexOf(todo);
+
+        document.querySelector('#todoTitle').value = todo.title;
+        document.querySelector('#todoDesc').value = todo.description;
+        document.querySelector('#todoProjectList').value = todo.projectName;
+        document.querySelector('#todoPriorities').value = todo.priority;
+        document.querySelector('#datepicker').value = todo.dueDate;
+
+    }
 });
 
 document.getElementById('saveBtn').onclick = () => {
-  const title = document.querySelector('#todoTitle');
-  const desc = document.querySelector('#todoDesc');
-  const projectName = document.querySelector('#todoProjectList');
-  const priorityLevel = document.querySelector('#todoPriorities');
-  const dueDate = document.querySelector('#datepicker');
-  const id = JSON.parse(localStorage.getItem('todo-collection')) ? JSON.parse(localStorage.getItem('todo-collection')).length + 1 : 0;
-  const todo = new Todo(
-    id, title.value, desc.value, dueDate.value, priorityLevel.value, projectName.value,
-  );
+    const title = document.querySelector('#todoTitle');
+    const desc = document.querySelector('#todoDesc');
+    const projectName = document.querySelector('#todoProjectList');
+    const priorityLevel = document.querySelector('#todoPriorities');
+    const dueDate = document.querySelector('#datepicker');
+    const id = JSON.parse(localStorage.getItem('todo-collection')) ? JSON.parse(localStorage.getItem('todo-collection')).length + 1 : 0;
 
-  saveNewToDo(todo);
-  resetFields(title, desc, projectName, priorityLevel, dueDate);
-  // eslint-disable-next-line no-undef
-  $('#todoModal').modal('hide');
+    const todo = new Todo(
+        id, title.value, desc.value, dueDate.value, priorityLevel.value, projectName.value,
+    );
+
+    saveNewToDo(todo);
+    resetFields(title, desc, projectName, priorityLevel, dueDate);
+    // eslint-disable-next-line no-undef
+    $('#todoModal').modal('hide');
 };
