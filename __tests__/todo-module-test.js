0e0fd2929b0f73df7c86ff788dc9ac968bbc6803
George <71434803+george-swift@users.noreply.github.com>
2021-05-31 22:42:46 +0100

    Add tests for todo module

--- __tests__/todo-module-test.js
+++ __tests__/todo-module-test.js
@@ -2,71 +2,69 @@
  * @jest-environment jsdom
  */
 
-import { createCards, Todo } from '../src/module/todoModule';
+import { createCards, Todo } from '../src/module/todoModule.js';
 
 describe('project module - todo', () => {
+  const listOfTodos = [];
+  listOfTodos.push(new Todo(1, 'The name of the boy is Blessing', 'Description here', '2021-06-02', 'High', 'Yusif'));
 
+  test('creates title', () => {
+    const div = document.createElement('div');
+    div.id = 'todo-app';
+    div.appendChild(createCards(listOfTodos));
+    const card = div.querySelector('.card');
+    expect(card.querySelector('.item-name').innerHTML).toMatch(/The name of the boy is Blessing/);
+  });
+
+  test('creates todo priority', () => {
+    const div = document.createElement('div');
+    div.id = 'todo-app';
+    div.appendChild(createCards(listOfTodos));
+    const badge = div.querySelector('.badge');
+    expect(badge.innerHTML).toMatch(/High/);
+  });
+
+  test('creates todo date', () => {
+    const div = document.createElement('div');
+    div.id = 'todo-app';
+    div.appendChild(createCards(listOfTodos));
+    const itemDate = div.querySelector('.item-date');
+    expect(itemDate.innerHTML).toMatch(/2021-06-02/);
+  });
+
+  test('creates todo description', () => {
+    const div = document.createElement('div');
+    div.id = 'todo-app';
+    div.appendChild(createCards(listOfTodos));
+    const itemDesc = div.querySelector('.item-desc');
+    expect(itemDesc.innerHTML).toMatch(/Description here/);
+  });
+
+  test('creates edit and delete buttons', () => {
+    const div = document.createElement('div');
+    div.id = 'todo-app';
+    div.appendChild(createCards(listOfTodos));
+    const footer = div.querySelector('.card-footer');
+    expect(footer.querySelector('.btn-secondary').innerHTML).toMatch(/Edit/);
+    expect(footer.querySelector('.btn-danger').innerHTML).toMatch(/Delete/);
+    expect(footer.querySelectorAll('button').length).toEqual(2);
+  });
+
+  describe('project module - list of cards', () => {
     const listOfTodos = [];
-    listOfTodos.push(new Todo(1, 'The name of the boy is Blessing', 'Description here', '2021-06-02', 'High', 'Yusif'));
 
-    test('creates title', () => {
-        const div = document.createElement('div');
-        div.id = 'todo-app';
-        div.appendChild(createCards(listOfTodos));
-        const card = div.querySelector('.card');
-        expect(card.querySelector('.item-name').innerHTML).toMatch(/The name of the boy is Blessing/);
+    beforeAll(() => {
+      for (let i = 0; i < 5; i += 1) {
+        listOfTodos.push(new Todo(i + 1, 'The name of the boy is Blessing', 'Description here', '2021-06-02', 'High', 'Yusif'));
+      }
     });
 
-    test('creates todo priority', () => {
-        const div = document.createElement('div');
-        div.id = 'todo-app';
-        div.appendChild(createCards(listOfTodos));
-        const badge = div.querySelector('.badge');
-        expect(badge.innerHTML).toMatch(/High/);
+    test('creates all cards', () => {
+      const div = document.createElement('div');
+      div.id = 'todo-app';
+      div.appendChild(createCards(listOfTodos));
+      const cards = div.querySelectorAll('.card');
+      expect(cards.length).toEqual(5);
     });
-
-    test('creates todo date', () => {
-        const div = document.createElement('div');
-        div.id = 'todo-app';
-        div.appendChild(createCards(listOfTodos));
-        const itemDate = div.querySelector('.item-date');
-        expect(itemDate.innerHTML).toMatch(/2021-06-02/);
-    });
-
-    test('creates todo description', () => {
-        const div = document.createElement('div');
-        div.id = 'todo-app';
-        div.appendChild(createCards(listOfTodos));
-        const itemDesc = div.querySelector('.item-desc');
-        expect(itemDesc.innerHTML).toMatch(/Description here/);
-    });
-
-    test('creates edit and delete buttons', () => {
-        const div = document.createElement('div');
-        div.id = 'todo-app';
-        div.appendChild(createCards(listOfTodos));
-        const footer = div.querySelector('.card-footer');
-        expect(footer.querySelector('.btn-secondary').innerHTML).toMatch(/Edit/);
-        expect(footer.querySelector('.btn-danger').innerHTML).toMatch(/Delete/);
-        expect(footer.querySelectorAll('button').length).toEqual(2);
-    });
-
-    describe('project module - list of cards', () => {
-        const listOfTodos = [];
-
-        beforeAll(() => {
-            for (let i = 0; i < 5; i += 1) {
-                listOfTodos.push(new Todo(i + 1, 'The name of the boy is Blessing', 'Description here', '2021-06-02', 'High', 'Yusif'));
-            }
-        });
-
-        test('creates all cards', () => {
-            const div = document.createElement('div');
-            div.id = 'todo-app';
-            div.appendChild(createCards(listOfTodos));
-            const cards = div.querySelectorAll('.card');
-            expect(cards.length).toEqual(5);
-        });
-    });
-
-})
+  });
+});
--- src/module/todoModule.js
+++ src/module/todoModule.js
@@ -1,87 +1,87 @@
 import attributes from './domManager.js';
 
 function Todo(id, title, description, dueDate, priority, project) {
-    this.id = id;
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
-    this.projectName = project;
+  this.id = id;
+  this.title = title;
+  this.description = description;
+  this.dueDate = dueDate;
+  this.priority = priority;
+  this.projectName = project;
 }
 
 function defineAttributes(elem, attributes) {
-    Object.entries(attributes).forEach(([key, value]) => {
-        elem.setAttribute(key, value);
-    });
+  Object.entries(attributes).forEach(([key, value]) => {
+    elem.setAttribute(key, value);
+  });
 }
 
 function createCards(todos) {
-    const cardsContainer = document.createElement('div');
-    cardsContainer.id = 'card-container';
+  const cardsContainer = document.createElement('div');
+  cardsContainer.id = 'card-container';
 
-    for (let i = 0; i < todos.length; i += 1) {
-        const card = document.createElement('div');
-        defineAttributes(card, attributes.card.wrap);
+  for (let i = 0; i < todos.length; i += 1) {
+    const card = document.createElement('div');
+    defineAttributes(card, attributes.card.wrap);
 
-        const cardHeader = document.createElement('div');
-        defineAttributes(cardHeader, attributes.card.header);
+    const cardHeader = document.createElement('div');
+    defineAttributes(cardHeader, attributes.card.header);
 
-        const itemTitle = document.createElement('h5');
-        defineAttributes(itemTitle, attributes.card.title);
+    const itemTitle = document.createElement('h5');
+    defineAttributes(itemTitle, attributes.card.title);
 
-        const itemDetails = document.createElement('div');
-        defineAttributes(itemDetails, attributes.card.details);
+    const itemDetails = document.createElement('div');
+    defineAttributes(itemDetails, attributes.card.details);
 
-        const itemDate = document.createElement('p');
-        defineAttributes(itemDate, attributes.card.date);
+    const itemDate = document.createElement('p');
+    defineAttributes(itemDate, attributes.card.date);
 
-        const itemPriority = document.createElement('span');
-        defineAttributes(itemPriority, attributes.card.badge[`${todos[i].priority.toLowerCase()}`]);
+    const itemPriority = document.createElement('span');
+    defineAttributes(itemPriority, attributes.card.badge[`${todos[i].priority.toLowerCase()}`]);
 
-        itemDetails.append(itemPriority, itemDate);
-        cardHeader.append(itemTitle, itemDetails);
+    itemDetails.append(itemPriority, itemDate);
+    cardHeader.append(itemTitle, itemDetails);
 
-        const cardBody = document.createElement('div');
-        defineAttributes(cardBody, attributes.card.body);
+    const cardBody = document.createElement('div');
+    defineAttributes(cardBody, attributes.card.body);
 
-        const itemDescription = document.createElement('p');
-        defineAttributes(itemDescription, attributes.card.desc);
+    const itemDescription = document.createElement('p');
+    defineAttributes(itemDescription, attributes.card.desc);
 
-        cardBody.appendChild(itemDescription);
+    cardBody.appendChild(itemDescription);
 
-        const cardFooter = document.createElement('div');
-        defineAttributes(cardFooter, attributes.card.footer);
+    const cardFooter = document.createElement('div');
+    defineAttributes(cardFooter, attributes.card.footer);
 
-        const editButton = document.createElement('button');
-        defineAttributes(editButton, attributes.card.editButton);
-        editButton.textContent = 'Edit';
-        editButton.id = `edit-${todos[i].id}`;
+    const editButton = document.createElement('button');
+    defineAttributes(editButton, attributes.card.editButton);
+    editButton.textContent = 'Edit';
+    editButton.id = `edit-${todos[i].id}`;
 
-        const deleteButton = document.createElement('button');
-        defineAttributes(deleteButton, attributes.card.deleteButton);
-        deleteButton.textContent = 'Delete';
-        deleteButton.id = `delete-${todos[i].id}`;
+    const deleteButton = document.createElement('button');
+    defineAttributes(deleteButton, attributes.card.deleteButton);
+    deleteButton.textContent = 'Delete';
+    deleteButton.id = `delete-${todos[i].id}`;
 
-        cardFooter.append(editButton, deleteButton);
+    cardFooter.append(editButton, deleteButton);
 
-        Object.entries(todos[i]).forEach(([key, value]) => {
-            if (key === 'title') {
-                itemTitle.textContent = value;
-            } else if (key === 'description') {
-                itemDescription.textContent = value;
-            } else if (key === 'dueDate') {
-                itemDate.textContent = value;
-            } else if (key === 'priority') {
-                itemPriority.textContent = value;
-            }
-        });
+    Object.entries(todos[i]).forEach(([key, value]) => {
+      if (key === 'title') {
+        itemTitle.textContent = value;
+      } else if (key === 'description') {
+        itemDescription.textContent = value;
+      } else if (key === 'dueDate') {
+        itemDate.textContent = value;
+      } else if (key === 'priority') {
+        itemPriority.textContent = value;
+      }
+    });
 
-        card.append(cardHeader, cardBody, cardFooter);
+    card.append(cardHeader, cardBody, cardFooter);
 
-        cardsContainer.appendChild(card);
-    }
+    cardsContainer.appendChild(card);
+  }
 
-    return cardsContainer;
+  return cardsContainer;
 }
 
 export { Todo, createCards };
