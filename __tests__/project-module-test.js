30ae201008d8af4a5da731575c3276d9db8e29a5
George <71434803+george-swift@users.noreply.github.com>
2021-05-31 18:37:48 +0100

    Add test for new project button

--- __tests__/project-module-test.js
+++ __tests__/project-module-test.js
@@ -1,19 +1,29 @@
- /**
+/**
   * @jest-environment jsdom
   */
 
- import { createDefaultProject } from '../src/module/projectModule';
+import { newProjectButton, createDefaultProject } from '../src/module/projectModule';
 
- describe('project module', () => {
-     const menuList = document.createElement('ul');
+describe('project module', () => {
+  const menuList = document.createElement('ul');
 
-     beforeAll(() => {
-         const defaultProject = createDefaultProject('Default');
-         menuList.appendChild(defaultProject);
-     })
+  beforeAll(() => {
+    const defaultProject = createDefaultProject('Default');
+    const newProjectBtn = newProjectButton();
+    menuList.append(defaultProject, newProjectBtn);
+  });
 
-     test('creates default project', () => {
-         const projectList = menuList.querySelector('#Default');
-         expect(projectList.id).toBe('Default');
-     });
- });
+  test('creates default project', () => {
+    const projectList = menuList.querySelector('#Default');
+    expect(projectList.id).toMatch(/Default/);
+    const project = menuList.querySelector('a');
+    expect(project.classList).toContain('project-name-default');
+  });
+
+  test('creates a new project button', () => {
+    const newProjectBtn = menuList.querySelector('#newProjectBtn');
+    expect(newProjectBtn).not.toBeNull();
+    const action = newProjectBtn.querySelector('span');
+    expect(action.textContent).toMatch(/Add new project/);
+  });
+});
